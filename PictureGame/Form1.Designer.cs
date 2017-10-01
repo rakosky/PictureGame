9982fa88226697c060fdf5ff41a8889708df02b3
rakosky <31675033+rakosky@users.noreply.github.com>
2017-10-01 13:30:41 -0400

    Initial commit

---
+++ PictureGame/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace PictureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.loadImgToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearImgToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Outset;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 27);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(600, 600);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImgToolStripMenuItem,
+            this.clearImgToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(628, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // loadImgToolStripMenuItem
+            // 
+            this.loadImgToolStripMenuItem.Name = "loadImgToolStripMenuItem";
+            this.loadImgToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.loadImgToolStripMenuItem.Text = "Load Img";
+            this.loadImgToolStripMenuItem.Click += new System.EventHandler(this.loadImgToolStripMenuItem_Click);
+            // 
+            // clearImgToolStripMenuItem
+            // 
+            this.clearImgToolStripMenuItem.Name = "clearImgToolStripMenuItem";
+            this.clearImgToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.clearImgToolStripMenuItem.Text = "Clear Img";
+            this.clearImgToolStripMenuItem.Click += new System.EventHandler(this.clearImgToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 634);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lul";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadImgToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearImgToolStripMenuItem;
+    }
+}
+
---
+++ PictureGame/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap[] origImgPieces;
+
+        private Bitmap[] chopBmp(Bitmap b)
+        {
+            Bitmap[] bArray = new Bitmap[8];
+            for(int i = 0; i < 8; i++)
+            {
+                Bitmap target = new Bitmap(200,200);
+
+                using (Graphics g = Graphics.FromImage(target))
+                {
+                    g.DrawImage(b, new Rectangle(0, 0, target.Width, target.Height), new Rectangle((i%3)*(b.Width/3), (i/3) * (b.Height / 3), 200,200), GraphicsUnit.Pixel);
+                    bArray[i] = target;
+                }
+            }
+
+            return bArray;
+        }
+
+        private void loadImgToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap img;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Files|*.jpg;*.jpeg;*.png;";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    img = new Bitmap((Bitmap)Image.FromFile(openFileDialog1.FileName), 600,600);
+                    origImgPieces = chopBmp(img);//unswapped
+                    Random rnd = new Random();
+                    Bitmap[] imgPieces = origImgPieces;//.OrderBy(x => rnd.Next()).ToArray();
+                    for (int i = 0; i < 9; i++)
+                    {
+                        PictureBox p = new PictureBox();
+                        p.Click += new System.EventHandler(this.imgSquare_Click);
+                        this.tableLayoutPanel1.Controls.Add(p);
+                        p.SetBounds(0, 0, p.Parent.Width, p.Parent.Height);
+                        
+                        if (i < 8)
+                            p.Image = imgPieces[i];
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Bad File");
+                }
+            }
+            
+        }
+
+        private void imgSquare_Click(object sender, EventArgs e)
+        {
+            PictureBox s = ((PictureBox)sender);
+            int srcRow = this.tableLayoutPanel1.GetPositionFromControl(s).Row;
+            int srcCol = this.tableLayoutPanel1.GetPositionFromControl(s).Column;
+            PictureBox target;
+            if((srcRow+1<=tableLayoutPanel1.RowCount-1 && (target = (PictureBox)tableLayoutPanel1.GetControlFromPosition(srcCol, srcRow + 1)).Image == null) || 
+                (srcRow - 1 >= 0 && (target = (PictureBox)tableLayoutPanel1.GetControlFromPosition(srcCol, srcRow - 1)).Image == null) ||
+                (srcCol + 1 <= tableLayoutPanel1.RowCount - 1 && (target = (PictureBox)tableLayoutPanel1.GetControlFromPosition(srcCol + 1, srcRow)).Image == null) ||
+                (srcCol - 1 >= 0 && (target = (PictureBox)tableLayoutPanel1.GetControlFromPosition(srcCol - 1, srcRow)).Image == null))
+            {
+                Image temp = null;
+                temp = s.Image;
+                s.Image = target.Image;
+                target.Image = temp;
+            }
+            //check if finished
+            bool done = false;
+            for(int i = 0; i < 8; i++)
+            {
+                if (origImgPieces[i] != ((PictureBox)tableLayoutPanel1.GetControlFromPosition(i%3, i/3)).Image)
+                {
+                    break;
+                }
+                if (i == 7)
+                {
+                    MessageBox.Show("Nice job ;]");
+                }
+            }
+        }
+
+        private void clearImgToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.tableLayoutPanel1.Controls.Clear();
+        }
+    }
+}
